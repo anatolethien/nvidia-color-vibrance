96f03ad53241a7a5105829486f5d55fb702a7d32
anatolethien <anatole.thien@ynov.com>
2021-11-23 23:09:05 +0100

    Watermark

--- nvidia-color-vibrance.sh
+++ nvidia-color-vibrance.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script written by @anatolethien
+
 CURRENT_VIBRANCE=$(nvidia-settings --query DigitalVibrance | grep --max-count=1 Attribute | awk '{ print $4 }' | tr -d '.')
 
 echo "Current color vibrance: $CURRENT_VIBRANCE"
@@ -9,3 +11,4 @@ read -p "Enter new color vibrance value (between -1024 and 1023): " VIBRANCE
 nvidia-settings --assign DigitalVibrance=$VIBRANCE > /dev/null
 
 echo "New color vibrance: $VIBRANCE"
+
